221c225b2cf37178607d93c610cd000714e8574c
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-08-09 21:41:38 +0530

    Create AdminJobPortalSystemApplication.java

---
+++ src/main/java/com/example/admin_job_portal_system/AdminJobPortalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.admin_job_portal_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminJobPortalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminJobPortalSystemApplication.class, args);
+	}
+
+}
