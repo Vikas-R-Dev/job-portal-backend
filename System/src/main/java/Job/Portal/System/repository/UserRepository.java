7a085fe02840669e30e605d01ca9450ee6cdbb30
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-09-04 19:31:47 +0530

    Create UserRepository.java

---
+++ System/src/main/java/Job/Portal/System/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package Job.Portal.System.repository;
+
+import Job.Portal.System.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    User findByEmail(String email);
+}
