6f48ee8801e058e782aa9afddb7d66ac53014c4f
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-09-04 19:33:24 +0530

    Create UserServiceImpl.java

---
+++ System/src/main/java/Job/Portal/System/service/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package Job.Portal.System.service;
+
+import Job.Portal.System.model.User;
+import Job.Portal.System.repository.UserRepository;
+import Job.Portal.System.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
