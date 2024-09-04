fc85f92a39526bd5754e6853bec541920043648d
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-09-04 19:27:45 +0530

    Delete JobRepository.java

--- src/main/java/com/example/admin_job_portal_system/repository/JobRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.admin_job_portal_system.repository;
-
-import com.example.admin_job_portal_system.entity.Job;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JobRepository extends JpaRepository<Job, Long> {
-}
