7273ff2756062f88a44312489568598d0f7b2655
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-09-04 19:28:48 +0530

    Delete JobPositionRepository.java

--- src/main/java/com/example/Admin_Dashboard_backend/repository/JobPositionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Admin_Dashboard_backend.repository;
-
-import com.example.Admin_Dashboard_backend.model.JobPosition;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobPositionRepository extends JpaRepository<JobPosition, Long> {
-}
