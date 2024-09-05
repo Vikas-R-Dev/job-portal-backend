7bfb2c3e1110adc304ee2512e2bb0500336e1531
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-09-05 13:28:16 +0530

    Update JobCategoryRepository.java

--- System/src/main/java/Job/Portal/System/repository/JobCategoryRepository.java
+++ System/src/main/java/Job/Portal/System/repository/JobCategoryRepository.java
@@ -4,7 +4,17 @@ import Job.Portal.System.model.JobCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for {@link JobCategory} entity.
+ * Provides methods to interact with the JobCategory data source.
+ */
 @Repository
 public interface JobCategoryRepository extends JpaRepository<JobCategory, Long> {
+    /**
+     * Find a job category by its name.
+     *
+     * @param name the name of the job category
+     * @return the job category with the given name
+     */
     JobCategory findByName(String name);
 }
