e04877e1b57f3a4ea1e2d2028eeadfe989418a66
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-09-05 13:28:38 +0530

    Update JobApplicationService.java

--- System/src/main/java/Job/Portal/System/service/JobApplicationService.java
+++ System/src/main/java/Job/Portal/System/service/JobApplicationService.java
@@ -7,9 +7,18 @@ import Job.Portal.System.model.Job;
 import java.util.List;
 
 public interface JobApplicationService {
+    // Method to apply for a job
     JobApplication applyForJob(JobApplication jobApplication);
+
+    // Method to get job applications by job seeker (user)
     List<JobApplication> getJobApplicationsByJobSeeker(User jobSeeker);
+
+    // Method to get job applications by job
     List<JobApplication> getJobApplicationsByJob(Job job);
+
+    // Method to get all job applications
     List<JobApplication> getAllJobApplications();
+
+    // Method to update the status of a job application by its ID
     JobApplication updateJobApplicationStatus(Long id, JobApplication.Status status);
 }
