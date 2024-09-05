b20055f1a7d23ca714dfe31ae1dc7c05cbcc295d
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-09-05 13:28:13 +0530

    Update JobApplicationRepository.java

--- System/src/main/java/Job/Portal/System/repository/JobApplicationRepository.java
+++ System/src/main/java/Job/Portal/System/repository/JobApplicationRepository.java
@@ -8,8 +8,25 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * Repository interface for {@link JobApplication} entity.
+ * Provides methods to interact with the JobApplication data source.
+ */
 @Repository
 public interface JobApplicationRepository extends JpaRepository<JobApplication, Long> {
+    /**
+     * Find all job applications by the job seeker.
+     *
+     * @param jobSeeker the user who applied for the job
+     * @return a list of job applications made by the job seeker
+     */
     List<JobApplication> findByJobSeeker(User jobSeeker);
+
+    /**
+     * Find all job applications for a specific job.
+     *
+     * @param job the job for which applications are to be found
+     * @return a list of job applications for the specified job
+     */
     List<JobApplication> findByJob(Job job);
 }
